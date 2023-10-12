0991196e33df372b063026e3389b62487b2093fb
Priscr23 <Priscila.cervantes@iteso.mx>
2023-10-12 11:33:10 -0600

    1

--- convertir_a_binario.py
+++ convertir_a_binario.py
@@ -1,15 +1,22 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Diseña un programa para convertir un número base 10 a binario.
+750722
 """
+#Entradas
+numero = int(input("Introduzca un múmero: "))
 
-# Declaraciones
-CONSTANTE = valor
+#Procedimientos
+binario = []
 
-# Entradas
-entrada = input()
+if numero == 0:
+    binario.append(0)
+else:
+    while numero > 0:
+        residuo = numero % 2
+        binario.insert(0, residuo)  # Insertar el residuo al principio de la lista
+        numero = numero // 2
 
-# Proceso
+# Imprimir el número binario
+print(f"El número {numero} en binario es {binario}")
 
 
-# Salidas
-print(salida)
